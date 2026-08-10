7c3b6c90d8708cad0045df9f6e4d6b64a4094eb1
tiyanzxbellas <tiyabbella@gmail.com>
2026-08-10 07:07:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  telegramBotToken: '6322738178:AAG2nxNLoO8upLC4QDY7I-A_rLVGSL6L4Kk',
+  telegramBotToken: '7299033162:AAGT2ncLL9cUBDV7280AHOcSezDo7BaCBh0',
   ai: {
     baseURL: 'https://betatestervueui2-b.hf.space/v1',
     apiKey: 'sk-843e3f05f05eacfe-55n2je-f2c2b844',
